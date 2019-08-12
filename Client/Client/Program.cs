3834e11deb730c89618817a55d49129344c927f3
RFNotebook\radiofisik <mail@radiofisik.ru>
2019-08-12 20:57:24 +0200

    simplest redis

---
+++ Client/Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("docker");
+            IDatabase db = redis.GetDatabase();
+
+            string value = "hello";
+            db.StringSet("world", value, TimeSpan.FromHours(2));
+
+            string valueFromRedis = db.StringGet("world");
+            Console.WriteLine(valueFromRedis);
+
+            db.KeyDelete("world");
+            string valueFromRedis2 = db.StringGet("world");
+            Console.WriteLine(valueFromRedis2);
+            Console.ReadKey();
+        }
+    }
+}
